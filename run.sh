23071e4d4864ee9a8c3d1426085f66fc4b61179c
3tilley <1138504+3tilley@users.noreply.github.com>
2023-11-14 22:05:18 +0000

    Write testing

--- run.sh
+++ run.sh
@@ -14,7 +14,15 @@ CONFIG_PATH=/data/options.json
 DEVICE=$(bashio::config 'device')
 BAUD_RATE=$(bashio::config 'baud_rate')
 
+echo "Testing write permissions"
+test -w ${DEVICE} && echo success || echo failure 
 
+test -w ${DEVICE} && success=true || success=false
+
+echo "Testing permissions for $(whoami)"
+if [[ $success == "false" ]] ; then
+    sudo chown $(whoami) ${DEVICE}
+fi
 #(cd hue-thief && python3 -c "import hue_thief; print(hue-thief)")
 
 #python3 bellows devices
