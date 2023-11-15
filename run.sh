33745060f1e5db62b69f441620f345c99ffafcb2
3tilley <1138504+3tilley@users.noreply.github.com>
2023-11-15 21:33:53 +0000

    Fix env var

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ echo "Hello, World!"
 #ls /data
 #cat ${CONFIG_PATH}
 
-DEVICE=$(bashio::config 'device' "${ENV_DEVICE}")
-BAUD_RATE=$(bashio::config 'baud_rate' "${ENV_BAUD_RATE}")
+DEVICE=$(bashio::config 'device' "${ENV_DEVICE:-null}")
+BAUD_RATE=$(bashio::config 'baud_rate' "${ENV_BAUD_RATE:-null}")
 
 echo "Testing write permissions"
 test -w ${DEVICE} && echo success || echo failure 
@@ -20,7 +20,7 @@ test -w ${DEVICE} && echo success || echo failure
 test -w ${DEVICE} && success=true || success=false
 
 echo "Testing permissions for $(whoami)"
-if [[ $success == "false" ]] && [[ ${ENV_CHECK_DEVICE} != "0" ]]; then
+if [[ $success == "false" ]] && [[ "${ENV_CHECK_DEVICE:-1}" != "0" ]]; then
     sudo chown $(whoami) ${DEVICE}
 fi
 #(cd hue-thief && python3 -c "import hue_thief; print(hue-thief)")
